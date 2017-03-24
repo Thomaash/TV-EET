3b24cec9ef179bce38d29922a540f31d5ce56043
tom.vycital@gmail.com <tom.vycital@gmail.com>
2017-03-24 17:11:46 +0100

    Zobraz dnešek při otevření historie

--- app/src/main/java/tomas_vycital/eet/android_app/history/HistoryFragment.java
+++ app/src/main/java/tomas_vycital/eet/android_app/history/HistoryFragment.java
@@ -13,16 +13,14 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
 
 import tomas_vycital.eet.android_app.MainActivity;
 import tomas_vycital.eet.android_app.R;
 import tomas_vycital.eet.android_app.RefreshableFragment;
 import tomas_vycital.eet.android_app.receipt.Receipt;
 import tomas_vycital.eet.android_app.receipt.Receipts;
-
-/**
- * Created by tom on 18.3.17.
- */
 
 public class HistoryFragment extends Fragment implements RefreshableFragment, CalendarView.OnDateChangeListener {
     private LinearLayout receipts;
@@ -48,18 +46,16 @@ public class HistoryFragment extends Fragment implements RefreshableFragment, Ca
         // Onclick listeners
         ((CalendarView) layout.findViewById(R.id.calendar)).setOnDateChangeListener(this);
 
-        // Inflate the layout for this fragment
+        // List today
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        this.listReceipts(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+
         return layout;
     }
 
     @Override
     public void refresh() {
-        // Continue only if the fragment is initialized
-        if (this.getView() == null) {
-            return;
-        }
-
-        this.receipts.removeAllViews();
     }
 
     @Override
@@ -69,6 +65,10 @@ public class HistoryFragment extends Fragment implements RefreshableFragment, Ca
 
     @Override
     public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int day) {
+        this.listReceipts(year, month, day);
+    }
+
+    private void listReceipts(int year, int month, int day) {
         this.receipts.removeAllViews();
         try {
             for (JSONObject jsonReceipt : Receipts.getReceipts(year, month, day)) {
